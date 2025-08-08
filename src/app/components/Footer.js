cb7f8bf029dcdccd7cc0d9a786ce8c454d5024e0
Huixia0210 <ooihuixia0210@gmail.com>
2025-08-08 12:04:18 +0800

    Update personal website footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,4 +1,4 @@
-import { Linkedin, Github, User, Mail, CodeXml } from "lucide-react";
+import { Linkedin, Github, Apple, Mail, CodeXml } from "lucide-react";
 
 
 
@@ -12,7 +12,7 @@ export default function Footer({ className }) {
     {
       name: "xiaohongshu",
       href: "https://www.xiaohongshu.com/user/profile/669d17c20000000024020af3",
-      icon: User,
+      icon: Apple,
     },
     {
       name: "linkedin",
