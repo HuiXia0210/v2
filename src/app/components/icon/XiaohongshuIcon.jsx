8cff8352b1bc63524f85f7c1b6e17c78ab8293c7
Huixia0210 <ooihuixia0210@gmail.com>
2025-08-08 11:47:32 +0800

    Update footer to include Xiaohongshu icon and link

--- src/app/components/icon/XiaohongshuIcon.jsx
+++ src/app/components/icon/XiaohongshuIcon.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function XiaohongshuIcon(props) {
   return (
     <Image
-      src="/icons/xiaohongshu.png" // path relative to public folder
+      src="/icon/xiaohongshu.png" // path relative to public folder
       alt="Xiaohongshu"
       width={20}
       height={20}
